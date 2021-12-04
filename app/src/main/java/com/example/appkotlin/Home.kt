1b5824537c25f9111fdfd5163dc5eae66a5592d8
Sroyart <94043858+Sroyart@users.noreply.github.com>
2021-12-04 16:36:40 +0100

    button listener

--- app/src/main/java/com/example/appkotlin/Home.kt
+++ app/src/main/java/com/example/appkotlin/Home.kt
@@ -2,13 +2,16 @@ package com.example.appkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class Home : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-        buttonArticle.setOnClickListener{
-
+        val buttonArticle = findViewById(R.id.buttonArticle) as Button
+        buttonArticle.setOnClickListener {
+            Toast.makeText(this@Home, "You clicked me.", Toast.LENGTH_SHORT).show()
         }
     }
 }
