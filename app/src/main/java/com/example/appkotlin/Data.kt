6e374bcac03efd2aeeed1a1a9f3ca00c181a30ad
Sroyart <94043858+Sroyart@users.noreply.github.com>
2021-12-23 17:15:38 +0100

    list d'items grace a RecyclerView

---
+++ app/src/main/java/com/example/appkotlin/Data.kt
@@ -0,0 +1,4 @@
+package com.example.appkotlin
+
+data class Data(var titleImage : Int, var heading : String)
+
--- app/src/main/java/com/example/appkotlin/Search.kt
+++ app/src/main/java/com/example/appkotlin/Search.kt
@@ -2,11 +2,47 @@ package com.example.appkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class Search : AppCompatActivity() {
+
+    private lateinit var newRecyclerview : RecyclerView
+    private lateinit var newArrayList : ArrayList<Data>
+    lateinit var imageId : Array<Int>
+    lateinit var heading : Array<String>
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
 
+
+        imageId = arrayOf(
+            R.drawable.a,
+            R.drawable.b,
+            R.drawable.c
+        )
+
+        heading = arrayOf(
+            "exemple a",
+            "exemple b",
+            "exemple c"
+        )
+
+        newRecyclerview = findViewById(R.id.recyclerview)
+        newRecyclerview.layoutManager = LinearLayoutManager(this)
+        newRecyclerview.setHasFixedSize(true)
+
+        newArrayList = arrayListOf<Data>()
+        getUserData()
+    }
+
+    private fun getUserData() {
+        for (i in imageId.indices) {
+            val data = Data(imageId[i],heading[i])
+            newArrayList.add(data)
+        }
+        newRecyclerview.adapter = MyAdapter(newArrayList)
     }
 }
