eb8d314b30879aec62bac51587724b10c405f2c8
Sroyart <94043858+Sroyart@users.noreply.github.com>
2022-02-01 19:16:40 +0100

    search

--- app/src/main/java/com/example/appkotlin/Search.kt
+++ app/src/main/java/com/example/appkotlin/Search.kt
@@ -3,14 +3,19 @@ package com.example.appkotlin
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.widget.SearchView
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import java.util.*
+import kotlin.collections.ArrayList
 
 class Search : AppCompatActivity() {
 
     private lateinit var newRecyclerview : RecyclerView
     private lateinit var newArrayList : ArrayList<Data>
+    private lateinit var tempArrayList: ArrayList<Data>
     lateinit var imageId : Array<Int>
     lateinit var heading : Array<String>
     lateinit var products : Array<String>
@@ -45,7 +50,44 @@ class Search : AppCompatActivity() {
         newRecyclerview.setHasFixedSize(true)
 
         newArrayList = arrayListOf<Data>()
+        tempArrayList = arrayListOf<Data>()
         getUserData()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        menuInflater.inflate(R.menu.menu_item,menu)
+        val item = menu?.findItem(R.id.search_action)
+        val searchView = item?.actionView as SearchView
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                TODO("Not yet implemented")
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+
+                tempArrayList.clear()
+                val searchText = newText!!.toLowerCase(Locale.getDefault())
+                if (searchText.isNotEmpty()){
+                    newArrayList.forEach{
+                        if (it.heading.toLowerCase(Locale.getDefault()).contains(searchText)){
+                            tempArrayList.add(it)
+                        }
+                    }
+
+                    newRecyclerview.adapter!!.notifyDataSetChanged()
+                }else{
+                    tempArrayList.clear()
+                    tempArrayList.addAll(newArrayList)
+                    newRecyclerview.adapter!!.notifyDataSetChanged()
+                }
+
+                return false
+
+            }
+        })
+
+        return super.onCreateOptionsMenu(menu)
     }
 
     private fun getUserData() {
@@ -54,7 +96,9 @@ class Search : AppCompatActivity() {
             newArrayList.add(data)
         }
 
-        var adapter = MyAdapter(newArrayList)
+        tempArrayList.addAll(newArrayList)
+
+        var adapter = MyAdapter(tempArrayList)
         newRecyclerview.adapter = adapter
         adapter.setOnItemClickListener(object : MyAdapter.onItemClickListener{
             override fun onItemClick(position: Int) {
